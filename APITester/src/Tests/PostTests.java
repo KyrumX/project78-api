9513e4ae0c15279aeb36949ac975514c377bcd54
Aaron <kyrumx@hotmail.nl>
2018-05-30 20:48:26 +0200

    Switched IP

--- APITester/src/Tests/PostTests.java
+++ APITester/src/Tests/PostTests.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PostTests {
-    public static void postTest(String url, String body) throws Exception {
+    public static void postTest(String url, String parameters) throws Exception {
         //Code is based on the frame provided by mkyong: https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
 
         System.out.println();
@@ -21,22 +21,18 @@ public class PostTests {
         connection.setRequestProperty("User-Agent", "Mozilla/5.0");
         connection.setRequestProperty("Accept", "application/json");
 
-        Date mydate = new Date();
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        String mydateStr = df.format(mydate);
 
-        String urlParameters = "tablenumber=111&datetime="+mydateStr+"";
 
         // Send post request
         connection.setDoOutput(true);
         DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-        wr.writeBytes(urlParameters);
+        wr.writeBytes(parameters);
         wr.flush();
         wr.close();
 
         int responseCode = connection.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Sending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + parameters);
         System.out.println("Response Code : " + responseCode);
 
 
@@ -58,4 +54,55 @@ public class PostTests {
         System.out.println(response.toString());
 
     }
+
+//    public static void postTest(String url, String body) throws Exception {
+//        //Code is based on the frame provided by mkyong: https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+//
+//        System.out.println();
+//        System.out.println("Starting a new POST test:");
+//
+//        URL object = new URL(url);
+//        HttpURLConnection connection = (HttpURLConnection) object.openConnection();
+//
+//        connection.setRequestMethod("POST");
+//        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+//        connection.setRequestProperty("Accept", "application/json");
+//
+//        Date mydate = new Date();
+//        DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+//        String mydateStr = df.format(mydate);
+//
+//        String urlParameters = "tablenumber=111&datetime="+mydateStr+"";
+//
+//        // Send post request
+//        connection.setDoOutput(true);
+//        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+//        wr.writeBytes(urlParameters);
+//        wr.flush();
+//        wr.close();
+//
+//        int responseCode = connection.getResponseCode();
+//        System.out.println("\nSending 'POST' request to URL : " + url);
+//        System.out.println("Post parameters : " + urlParameters);
+//        System.out.println("Response Code : " + responseCode);
+//
+//
+//        InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+//        if (responseCode != 201) {
+//            inputStreamReader = new InputStreamReader(connection.getErrorStream());
+//        }
+//
+//        BufferedReader in = new BufferedReader(inputStreamReader);
+//        String inputLine;
+//        StringBuffer response = new StringBuffer();
+//
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//
+//        //print result
+//        System.out.println(response.toString());
+//
+//    }
 }
